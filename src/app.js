56373a76c5f5660095b952970c7e43d46e657702
Brian Moya <122998981+brianmoyar89@users.noreply.github.com>
2023-05-05 17:32:34 +0000

    entrega final

--- src/app.js
+++ src/app.js
@@ -1,11 +1,29 @@
 /* eslint-disable */
 import "bootstrap";
 import "./style.css";
+let who = ["The dog", "My grandma", "His turtle", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "the keys", "the car"];
+let when = [
+  "before the class",
+  "right on time",
+  "when I finished",
+  "during my lunch",
+  "while I was praying"
+];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+function generateExcuse() {
+  let randomWho = who[Math.floor(Math.random() * who.length)];
+  let randomAction = action[Math.floor(Math.random() * action.length)];
+  let randomWhat = what[Math.floor(Math.random() * what.length)];
+  let randomWhen = when[Math.floor(Math.random() * when.length)];
+  return `${randomWho} ${randomAction} ${randomWhat} ${randomWhen}`;
+}
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+let btn = document.getElementById("generate-btn");
+let excuse = document.getElementById("excuse");
+
+btn.addEventListener("click", function() {
+  let newExcuse = generateExcuse();
+  excuse.innerHTML = newExcuse;
+});
